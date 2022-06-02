ffc283f8f97d92a6b312138820ae119a16c2d33b
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-06-02 15:26:44 -0500

    commit tree 5e79f36145eec4a78544e4fcbbdf6aae98bdfbb3

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+const employee = {
+    name: "Alie",
+    streetAddress: "7977 Marywood Dr"
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj, [key]: value
+    };
+};
+
+let newEmployee = updateEmployeeWithKeyAndValue(employee, "name", "Sam");
+updateEmployeeWithKeyAndValue(newEmployee, "streeAddress", "11 Broadway");
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Sam");
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {
+        ...obj
+    };
+    delete newObj[key];
+    return newObj;
+};
+
+newEmployee = deleteFromEmployeeByKey(employee, "name");
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+};
+
+destructivelyDeleteFromEmployeeByKey(employee, "name");
